f8ed0e6d14d67d088dc879aeaa3d42d23039441f
Vasista R <96642595+VasistaR@users.noreply.github.com>
2023-12-23 16:11:21 -0800

    Create stringfunctions.py

---
+++ bootcamp/stringfunctions.py
@@ -0,0 +1,24 @@
+name = "vasista"
+
+#length of a string
+print(len(name))
+
+#substrings (indiv. string)
+print(name[0])
+print(name[6])
+print(name[-2])
+
+#substrings (actual substrings)
+print(name[0:-2])
+print(name[0:4])
+print(name[:3])
+print(name[2:5])
+
+#returns whole string
+print(name[::])
+
+#step sizes
+print(name[::2])
+
+#reverse a string
+print(name[::-1])
