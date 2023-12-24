5d1c00611fee96bc116b5ac0809e07081d096be5
Vasista R <96642595+VasistaR@users.noreply.github.com>
2023-12-23 20:28:01 -0800

    Create print_formatting.py

---
+++ bootcamp/print_formatting.py
@@ -0,0 +1,6 @@
+print("hello world {}".format("is overused"))
+print("{0} {1} {3} {2}".format("hello", "world", "overused", "is"))
+print("{a} {b} {d} {c}".format(a = "hello", b = "world", c = "overused", d = "is"))
+a = 110/54
+print("the value of a is {b:0.3}".format(b = a))
+print(f"the value of a is {a}")
