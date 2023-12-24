b4b1e9823aaf470b3107470751c6ab4276e93353
Vasista R <96642595+VasistaR@users.noreply.github.com>
2023-12-23 23:42:29 -0800

    Create sets.py

---
+++ bootcamp/sets.py
@@ -0,0 +1,7 @@
+myset = set()
+print(myset)
+myset.add(1)
+print(myset)
+mylist = [1,1,1,1,2,3,3,4,5,6,7,8,8,9,9,8,9,9,9,9,9,9,9,9,5,3,2,5,4,5,6,3,5,7,8,4,3,4,5,4,5,6,7,5,6,8,5,2]
+myset = set(mylist)
+print(myset)
