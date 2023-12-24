5a306447155f114a1ff544ddb9487238b28669b0
Vasista R <96642595+VasistaR@users.noreply.github.com>
2023-12-23 23:14:37 -0800

    Create dictionaries.py

---
+++ bootcamp/dictionaries.py
@@ -0,0 +1,13 @@
+dict = {"name":"Vasista","age":16,"gender":"male"}
+print(dict["name"])
+newdict = {"newlist":[1,2,3,4],"seconddict":{1:"a",2:"b"}}
+print(newdict["newlist"][0])
+print(newdict["seconddict"][1])
+print(newdict["seconddict"][1].upper())
+dict["country"] = "USA"
+print(dict)
+dict["name"] = "Vasista R."
+print(dict)
+print(dict.values())
+print(dict.keys())
+print(dict.items())
