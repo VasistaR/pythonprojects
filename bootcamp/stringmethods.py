64493342f5f13e47928a6dc93a1005f20ae6363b
Vasista R <96642595+VasistaR@users.noreply.github.com>
2023-12-23 20:02:43 -0800

    Create stringmethods.py

---
+++ bootcamp/stringmethods.py
@@ -0,0 +1,12 @@
+string = "hello world"
+string = string + " is overused"
+print(string)
+string = string + string[::1]
+print(string)
+string = "z"
+string = string * 10
+print(string)
+print(string.upper())
+string = "hello world"
+print(string.split())
+print(string.split("o"))
