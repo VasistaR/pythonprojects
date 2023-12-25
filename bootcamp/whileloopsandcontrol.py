d8ba3bfe0df0526677f1048960f28468a896e11d
Vasista R <96642595+VasistaR@users.noreply.github.com>
2023-12-25 13:51:37 -0800

    Create whileloopsandcontrol.py

---
+++ bootcamp/whileloopsandcontrol.py
@@ -0,0 +1,14 @@
+x = 0
+while x <= 5:
+    print(x)
+    x += 1
+else:
+    print("x is not less than 5")
+for x in "Sammy":
+    if x == "a":
+        continue
+    print(x)
+for x in "Sammy":
+    if x == "a":
+        break
+    print(x)
