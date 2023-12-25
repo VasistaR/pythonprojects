da0a3059d590db05d293f6ab9e78228237c84396
Vasista R <96642595+VasistaR@users.noreply.github.com>
2023-12-25 12:57:02 -0800

    Create ifstatements.py

---
+++ bootcamp/ifstatements.py
@@ -0,0 +1,9 @@
+store = 1
+if store == 1:
+    print("i love gaming!")
+elif store == 2:
+    print("cars go vroom!")
+elif store == 3:
+    print("pets are very cute!")
+else:
+    print("you have to choose another one i guess!")
