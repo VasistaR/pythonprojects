933261e5ea2c4ddeddca35dfc142c6786f18d32b
Vasista R <96642595+VasistaR@users.noreply.github.com>
2023-12-27 19:47:45 -0800

    Create functionsintro.py

---
+++ bootcamp/functionsandmethods/functionsintro.py
@@ -0,0 +1,8 @@
+'''def keyword to create a function in python
+snake casing - lowercase letters and underscores as spacing'''
+def basicfunction(name):
+    print(f'hello {name}')
+basicfunction("vasista")
+def additionfunction(x,y):
+    return x + y
+print(additionfunction(1,2))
