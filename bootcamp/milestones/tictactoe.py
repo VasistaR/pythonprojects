6bd82a301a9e6ba4e972516bb836e610ac3a91e3
Vasista R <96642595+VasistaR@users.noreply.github.com>
2023-12-31 17:37:48 -0800

    Update tictactoe.py

--- bootcamp/milestones/tictactoe.py
+++ bootcamp/milestones/tictactoe.py
@@ -1,169 +1,129 @@
-choice = 0
-player1puck = "A"
-player2puck = "A"
-count = 0
-gamenotwon = True
-row1 = ["","",""]
-row2 = ["","",""]
-row3 = ["","",""]
-def checkgamewon():
-    if row1[0] == row1[1] and row1[1] == row1[2]:
-        gamenotwon = False
-    if row2[0] == row2[1] and row2[1] == row2[2]:
-        gamenotwon = False
-    if row3[0] == row3[1] and row3[1] == row3[2]:
-        gamenotwon = False
-    if row1[0] == row2[1] and row2[1] == row3[2]:
-        gamenotwon = False
-    if row3[0] == row2[1] and row2[1] == row1[2]:
-        gamenotwon = False
-    if row1[0] == row2[0] and row2[0] == row3[0]:
-        gamenotwon = False
-    if row1[1] == row2[1] and row2[1] == row3[1]:
-        gamenotwon = False
-    if row1[2] == row2[2] and row2[2] == row3[2]:
-        gamenotwon = False
-def move():
-    if player1orplayer2():
-        if player1puck == "X":
-            if choice == 1:
-                row3[0] = "X"
-                printboard()
-            if choice == 2:
-                row3[1] = "X"
-                printboard()
-            if choice == 3:
-                row3[2] = "X"
-                printboard()
-            if choice == 4:
-                row2[0] = "X"
-                printboard()
-            if choice == 5:
-                row2[1] = "X"
-                printboard()
-            if choice == 6:
-                row2[2] - "X"
-                printboard()
-            if choice == 7:
-                row1[0] - "X"
-                printboard()
-            if choice == 8:
-                row1[1] = "X"
-                printboard()
-            if choice == 9:
-                row1[2] = "X"
-                printboard()
-        if player1puck == "O":
-            if choice == 1:
-                row3[0] = "O"
-                printboard()
-            if choice == 2:
-                row3[1] = "O"
-                printboard()
-            if choice == 3:
-                row3[2] = "O"
-                printboard()
-            if choice == 4:
-                row2[0] = "O"
-                printboard()
-            if choice == 5:
-                row2[1] = "O"
-                printboard()
-            if choice == 6:
-                row2[2] - "O"
-                printboard()
-            if choice == 7:
-                row1[0] - "O"
-                printboard()
-            if choice == 8:
-                row1[1] = "O"
-                printboard()
-            if choice == 9:
-                row1[2] = "O"
-                printboard()
-    else:
-        if player2puck == "X":
-            if choice == 1:
-                row3[0] = "X"
-                printboard()
-            if choice == 2:
-                row3[1] = "X"
-                printboard()
-            if choice == 3:
-                row3[2] = "X"
-                printboard()
-            if choice == 4:
-                row2[0] = "X"
-                printboard()
-            if choice == 5:
-                row2[1] = "X"
-                printboard()
-            if choice == 6:
-                row2[2] - "X"
-                printboard()
-            if choice == 7:
-                row1[0] - "X"
-                printboard()
-            if choice == 8:
-                row1[1] = "X"
-                printboard()
-            if choice == 9:
-                row1[2] = "X"
-                printboard()
-        if player2puck == "O":
-            if choice == 1:
-                row3[0] = "O"
-                printboard()
-            if choice == 2:
-                row3[1] = "O"
-                printboard()
-            if choice == 3:
-                row3[2] = "O"
-                printboard()
-            if choice == 4:
-                row2[0] = "O"
-                printboard()
-            if choice == 5:
-                row2[1] = "O"
-                printboard()
-            if choice == 6:
-                row2[2] - "O"
-                printboard()
-            if choice == 7:
-                row1[0] - "O"
-                printboard()
-            if choice == 8:
-                row1[1] = "O"
-                printboard()
-            if choice == 9:
-                row1[2] = "O"
-                printboard()
-def x_or_o():
-    player1puck = input("Player1, do you choose the X or the O?: ")
-    if player1puck == "X":
-        player2puck = "O"
-    else:
-        player2puck = "X"
-def choice():
-    choice = int(input("Make a choice on the numpad: "))
-    return choice
-def player1orplayer2():
-    ## when count is even, it is player1's turn
-    if count % 2 == 0:
-        return True
-    else:
-        return False
 def starttictactoe():
+    choice = 0
+    player1puck = 5
+    player2puck = 6
+    count = 0
+    gamenotwon = True
+    player1orplayer2 = True
+    row1 = ["","",""]
+    row2 = ["","",""]
+    row3 = ["","",""]
     def printboard():
         print(row1)
         print("")
         print(row2)
         print("")
-        print(row3)
+        print(row3)        
+    def x_or_o():
+        player1puck = int(input("Player1, do you choose the X or the O? 1 for X and 2 for O: "))
+        if player1puck == 1:
+            player2puck = 2
+        else:
+            player2puck = 1
+            player1puck = 2
+    def move():
+        if player1orplayer2:
+            if player1puck == 1:
+                if choice == 1:
+                    row3[0] = "X"
+                if choice == 2:
+                    row3[1] = "X"
+                if choice == 3:
+                    row3[2] = "X"
+                if choice == 4:
+                    row2[0] = "X"
+                if choice == 5:
+                    row2[1] = "X"
+                if choice == 6:
+                    row2[2] = "X"
+                if choice == 7:
+                    row1[0] = "X"
+                if choice == 8:
+                    row1[1] = "X"
+                if choice == 9:
+                    row1[2] = "X"
+            if player1puck == 2:
+                if choice == 1:
+                    row3[0] = "O"
+                if choice == 2:
+                    row3[1] = "O"
+                if choice == 3:
+                    row3[2] = "O"
+                if choice == 4:
+                    row2[0] = "O"
+                if choice == 5:
+                    row2[1] = "O"
+                if choice == 6:
+                    row2[2] = "O"
+                if choice == 7:
+                    row1[0] = "O"
+                if choice == 8:
+                    row1[1] = "O"
+                if choice == 9:
+                    row1[2] = "O"
+        else:
+            if player2puck == 1:
+                if choice == 1:
+                    row3[0] = "X"
+                if choice == 2:
+                    row3[1] = "X"
+                if choice == 3:
+                    row3[2] = "X"
+                if choice == 4:
+                    row2[0] = "X"
+                if choice == 5:
+                    row2[1] = "X"
+                if choice == 6:
+                    row2[2] = "X"
+                if choice == 7:
+                    row1[0] = "X"
+                if choice == 8:
+                    row1[1] = "X"
+                if choice == 9:
+                    row1[2] = "X"
+            if player2puck == 2:
+                if choice == 1:
+                    row3[0] = "O"
+                if choice == 2:
+                    row3[1] = "O"
+                if choice == 3:
+                    row3[2] = "O"
+                if choice == 4:
+                    row2[0] = "O"
+                if choice == 5:
+                    row2[1] = "O"
+                if choice == 6:
+                    row2[2] = "O"
+                if choice == 7:
+                    row1[0] = "O"
+                if choice == 8:
+                    row1[1] = "O"
+                if choice == 9:
+                    row1[2] = "O"
+    def checkgamewon():
+        if row1[0] == row1[1] and row1[1] == row1[2]:
+            gamenotwon = False
+        if row2[0] == row2[1] and row2[1] == row2[2]:
+            gamenotwon = False
+        if row3[0] == row3[1] and row3[1] == row3[2]:
+            gamenotwon = False
+        if row1[0] == row2[1] and row2[1] == row3[2]:
+            gamenotwon = False
+        if row3[0] == row2[1] and row2[1] == row1[2]:
+            gamenotwon = False
+        if row1[0] == row2[0] and row2[0] == row3[0]:
+            gamenotwon = False
+        if row1[1] == row2[1] and row2[1] == row3[1]:
+            gamenotwon = False
+        if row1[2] == row2[2] and row2[2] == row3[2]:
+            gamenotwon = False
     printboard()
+    x_or_o()
     while gamenotwon:
-        choice()
+        choice = int(input("Make a choice on the numpad: "))
         move()
+        printboard()
+        player1orplayer2 = not player1orplayer2
         checkgamewon()
     if not gamenotwon:
         print("Congratulations you won!")
